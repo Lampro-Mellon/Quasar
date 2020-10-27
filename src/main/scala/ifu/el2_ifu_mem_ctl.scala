5875b0bcad2b4e44dc74a9c0e8fe2c74d66819f2
waleed-lm <waleed.ehsan@lampromellon.com>
2020-10-27 15:23:56 +0500

    IMC DONE

--- src/main/scala/ifu/el2_ifu_mem_ctl.scala
+++ src/main/scala/ifu/el2_ifu_mem_ctl.scala
@@ -129,6 +129,8 @@ class mem_ctl_bundle extends Bundle with el2_lib{
   val valids = Output(UInt())
   val tagv_mb_in = Output(UInt())
   val test = Output(UInt())
+  val test_way_status_out = Output(UInt())
+  val test_way_status_clken = Output(UInt())
 }
 class el2_ifu_mem_ctl extends Module with el2_lib {
   val io = IO(new mem_ctl_bundle)
@@ -724,7 +726,11 @@ class el2_ifu_mem_ctl extends Module with el2_lib {
     val way_status_out = Wire(Vec(ICACHE_TAG_DEPTH, UInt(ICACHE_STATUS_BITS.W)))
     for (i <- 0 until ICACHE_TAG_DEPTH / 8; j <- 0 until 8)
       way_status_out((8 * i) + j) := RegEnable(way_status_new_ff, 0.U, ((ifu_status_wr_addr_ff(2,0)===j.U) & way_status_wr_en_ff) & way_status_clken(i))
-    way_status := (0 until ICACHE_TAG_DEPTH).map(i => Fill(ICACHE_INDEX_HI - ICACHE_TAG_INDEX_LO, ifu_ic_rw_int_addr_ff === i.U) & way_status_out(i)).reverse.reduce(Cat(_, _))
+  val test_way_status_out = (0 until ICACHE_TAG_DEPTH).map(i=>way_status_out(i).asUInt).reverse.reduce(Cat(_,_))
+  io.test_way_status_out := test_way_status_out
+  val test_way_status_clken = (0 until ICACHE_TAG_DEPTH/8).map(i=>way_status_clken(i).asUInt()).reverse.reduce(Cat(_,_))
+  io.test_way_status_clken := test_way_status_clken
+  way_status := (0 until ICACHE_TAG_DEPTH).map(i => Fill(ICACHE_INDEX_HI - ICACHE_TAG_INDEX_LO, ifu_ic_rw_int_addr_ff === i.U) & way_status_out(i)).reverse.reduce(Cat(_, _))
     val ifu_ic_rw_int_addr_w_debug = Mux((io.ic_debug_rd_en | io.ic_debug_wr_en) & io.ic_debug_tag_array,
       io.ic_debug_addr(ICACHE_INDEX_HI - 3, ICACHE_TAG_INDEX_LO - 3), ifu_ic_rw_int_addr(ICACHE_INDEX_HI - 1, ICACHE_TAG_INDEX_LO - 1))
     ifu_ic_rw_int_addr_ff := withClock(io.free_clk) {
