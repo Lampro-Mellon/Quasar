4930ba7dca77356c5fc3d159416a93009f7783f7
waleed-lm <waleed.ehsan@lampromellon.com>
2020-12-10 16:50:23 +0500

    Lsu Updated

--- src/main/scala/quasar_wrapper.scala
+++ src/main/scala/quasar_wrapper.scala
@@ -197,7 +197,6 @@ class quasar_wrapper extends Module with lib with RequireAsyncReset {
   io.dma_hresp := core.io.dma_hresp
 
 }
-
 object QUASAR_Wrp extends App {
   println((new chisel3.stage.ChiselStage).emitVerilog(new quasar_wrapper()))
 }
