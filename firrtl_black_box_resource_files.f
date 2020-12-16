749d2729194a0757b3a7818e59a12144df2278f8
Abdul Hameed Akram <abdulhameed.akram@lampromellon.com>
2020-12-16 16:02:33 +0500

    verilog files

--- firrtl_black_box_resource_files.f
+++ firrtl_black_box_resource_files.f
@@ -1,3 +1,1 @@
-/home/waleedbinehsan/Desktop/Quasar-master/gated_latch.v
-/home/waleedbinehsan/Desktop/Quasar-master/dmi_wrapper.sv
-/home/waleedbinehsan/Desktop/Quasar-master/mem.sv
+/home/abdulhameed.akram/Videos/Quasar/gated_latch.v
