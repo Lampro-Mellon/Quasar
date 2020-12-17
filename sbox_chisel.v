c80dac6075c8c7e4f1155c0ce308520b775820cb
​Laraib Khan <laraib.khan@lampromellon.com>
2020-12-17 09:38:45 +0500

    PIC,param,lib,mem.scala added

--- sbox_chisel.v
+++
@@ -1,8 +0,0 @@
-module sbox_chisel(
-  input        clock,
-  input        reset,
-  input  [7:0] io_byte_in,
-  output [7:0] io_byte_out
-);
-  assign io_byte_out = 8'hc6; // @[cipher.scala 58:15]
-endmodule
