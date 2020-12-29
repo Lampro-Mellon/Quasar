ca17b6fcab561ac97a04d8b73d2525f31bcfc2ad
waleed-lm <waleed.ehsan@lampromellon.com>
2020-12-29 17:52:12 +0500

    MakeFile Updated

--- verif/sim/obj_dir/test_tb_top.d
+++
@@ -1,7 +0,0 @@
-test_tb_top.o: test_tb_top.cpp Vtb_top.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated_trace.h
