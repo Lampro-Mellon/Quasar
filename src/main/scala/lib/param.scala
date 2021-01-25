208045e9c3f6fdd20047564255ad76c4f3a5566d
​Laraib Khan <laraib.khan@lampromellon.com>
2021-01-25 11:46:37 +0500

    BP updated

--- src/main/scala/lib/param.scala
+++ src/main/scala/lib/param.scala
@@ -4,13 +4,13 @@ import chisel3.util._
 trait param {
   val BHT_ADDR_HI            = 	0x9
   val BHT_ADDR_LO            = 	0x2
-  val BHT_ARRAY_DEPTH        = 	0x010
+  val BHT_ARRAY_DEPTH        = 	0x100
   val BHT_GHR_HASH_1         = 	0x0
   val BHT_GHR_SIZE           = 	0x8
-  val BHT_SIZE               = 	0x010
+  val BHT_SIZE               = 	0x200
   val BTB_ADDR_HI            = 	0x09
   val BTB_ADDR_LO            = 	0x2
-  val BTB_ARRAY_DEPTH        = 	0x010
+  val BTB_ARRAY_DEPTH        = 	0x100
   val BTB_BTAG_FOLD          = 	0x0
   val BTB_BTAG_SIZE          = 	0x5
   val BTB_FOLD2_INDEX_HASH   = 	0x0
@@ -20,7 +20,7 @@ trait param {
   val BTB_INDEX2_LO          = 	0x0A
   val BTB_INDEX3_HI          = 	0x19
   val BTB_INDEX3_LO          = 	0x12
-  val BTB_SIZE               = 	0x010
+  val BTB_SIZE               = 	0x200
   val BUILD_AHB_LITE         = 	0x0
   val BUILD_AXI4             = 	0x1
   val BUILD_AXI_NATIVE       = 	0x1
