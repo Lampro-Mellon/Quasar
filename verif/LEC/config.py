031cee8e0a231b2c3529749123468086da282510
komaljaved-lm <komal.javed@lampromellon.com>
2021-04-02 12:59:53 +0500

    Updated LEC scripts

--- verif/LEC/config.py
+++ verif/LEC/config.py
@@ -1,39 +1,43 @@
+# Load Configurations for LEC of Golden and Revised Design
+
 import re
 infile= open("./design/snapshots/default/param.vh",'r')
 params = []
-lines = infile.readlines() 
+lines = infile.readlines() # Read Param.vh file.
 for line in lines:
 	patern_1=re.match(r'(.*):(.*)' , line )
 	if ((patern_1)):
-		lesson_group2=patern_1.group(1)
-		splittedl = lesson_group2.split()
-		split_data=''
-		for x in splittedl:
-  			split_data=split_data+" "+x
-		lesson_group3=patern_1.group(2)
-		splittedl2 = lesson_group3.split()
-		split_data2=''
-		for x in splittedl2:
-  			split_data2=split_data2+" "+x
+		# Group string to the left side of colon
+		patern_group1=patern_1.group(1)
+		split_bef_col = patern_group1.split()
+		bef_col=''
+		print("split_bef_col = ",split_bef_col)
+		for parametr in split_bef_col:
+			bef_col=bef_col+" "+parametr
+		# Group string to the right side of colon
+		patern_group2=patern_1.group(2)		
+		split_after_col = patern_group2.split()
+		after_col=''
+		for strng in split_after_col:
+			after_col=after_col+strng
+			
 	else:
 		continue
-	params.append(split_data+" = " + split_data2)
+	# concatenate the string obtained before and after colon
+	params.append(bef_col+" = " + after_col)
 
-#writing to a file
+# Writing configurations to file
 filename2 = "./verif/LEC/LEC_RTL/Golden_RTL/parameter.sv"
-#w+ tells python we are opening the file to write into it
 outfile = open(filename2, 'w+')
 outfile.write("#(parameter"+"\n")
 outfile.write("\t"+" AWIDTH = 7,"+"\n")
 outfile.write("\t"+" TAG = 1'h1,"+"\n")
-for x in params:
-	if ("DCCM_INDEX_BITS") in x:
-		y="// " + "DCCM_INDEX_BITS =  4'hC ,"
-		outfile.write("\t"+str(y)+"\n")
+for parameters in params:
+	if ("DCCM_INDEX_BITS") in parameters:
+		commented=" //" + parameters
+		outfile.write("\t"+str(commented)+"\n")
 	else:
-		outfile.write("\t"+str(x)+"\n")
+		outfile.write("\t"+str(parameters)+"\n")
 outfile.write(")"+"\n")
 outfile.close() #Close file
-print("Done...!")
-
-
+print("\nConfiguration file for LEC is updated successfully.\n")
